f42332597ce757fcf3543844c7f23c910fa76125
akshayMore2018 <37394442+akshayMore2018@users.noreply.github.com>
2018-10-03 06:21:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+MASTER=" " #user id to which bot listens to.
+TOKEN=" " #bots on discord are authenticated using a token. 
+
+#bot strings
+ACCESS_DENIED=["Dobby has only one master!","Dobby is busy at the moment.","I dont take orders from you, Sir."]
+ERROR=":x: **An error occurred!**"
+
+INVALID=["I dont understand what you are saying","Pardon?","I dont know what to say"]
+WELCOME=["Welcome to Solitude, traveller!","Greetings!","Hail fellow, well met."]
